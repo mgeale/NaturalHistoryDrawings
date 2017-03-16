4a31d737d34d1e5d70e31d2a26eb173f32c17f5d
Matt Geale <matthewgeale@me.com>
2017-03-17 09:45:17 +1100

    added lightbox open/close

--- app/app.modules.js
+++ app/app.modules.js
@@ -2,9 +2,32 @@ var app = angular.module('app', []);
 
 app.controller('drawings', function($scope, $http) {
 
-  $http.get('../data/data.json')
+  $http.get('data/data.json')
   .then(function(response) {
-    $scope.content = response.data;
+    $scope.imgs = response.data;
   });
+
+  var lightboxImg;
+  lightboxImg = document.getElementById('lightbox-img');
+
+  $scope.lClass = 'close';
+
+  var toggleClass = function() {
+    if ($scope.lClass == 'open') {
+      $scope.lClass = 'close';
+    } else {
+      $scope.lClass = 'open';
+    }
+  }
+
+  $scope.photoClick = function(event) {
+    console.log(this.img.highreslink);
+    toggleClass();
+  }
+
+  $scope.toggleClick = function() {
+    toggleClass();
+    lightboxImg.src = 'images/empty.svg';
+  }
 
 });
